41dd758600af9c7b0cf30bc34c878ff9f736c314
I-Kang Ding <ikding@gmail.com>
2020-09-15 21:53:52 -0700

    Format embed_tweet.py with PEP8 and black

--- pelican-plugins/embed_tweet/embed_tweet.py
+++ pelican-plugins/embed_tweet/embed_tweet.py
@@ -16,26 +16,29 @@ And also provides a link for Twitter username.
         will be replaced by a `Embedded-tweet`_ API.
 
 .. _Embedded-tweet: https://developer.twitter.com/en/docs/twitter-for-websites/embedded-tweets/overview.html
-
 """
 
-from pelican import signals
 import re
+
+from pelican import signals
 
 
 def embed_tweet(instance):
     if not instance._content:
         return
 
-    instance._content = re.sub(
-        r'(^|[^/])(t)@(\w{1,15})(\b[^\/])',
-        '\\1<a href="https://twitter.com/\\3">@\\3</a>\\4',
+    instance._content = (
         re.sub(
-            r'(^|[^/])(t)#(\w{1,15})/status/(\d+)\b',
-            '\\1<blockquote class="twitter-tweet" align="center"><a href="https://twitter.com/\\3/status/\\4">Tweet of \\3/\\4</a></blockquote>',
-            instance._content
+            r"(^|[^/])(t)@(\w{1,15})(\b[^\/])",
+            '\\1<a href="https://twitter.com/\\3">@\\3</a>\\4',
+            re.sub(
+                r"(^|[^/])(t)#(\w{1,15})/status/(\d+)\b",
+                '\\1<blockquote class="twitter-tweet" align="center"><a href="https://twitter.com/\\3/status/\\4">Tweet of \\3/\\4</a></blockquote>',
+                instance._content,
+            ),
         )
-    ) + '<script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
+        + '<script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
+    )
 
 
 def register():
